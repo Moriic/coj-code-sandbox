b123565fac4ade877511f72bb6bb4e34eaec30b4
Morii <2021694476@qq.com>
2024-08-30 15:40:58 +0800

    Update CojCodeSandboxApplicationTests.java

--- src/test/java/com/cwc/CojCodeSandboxApplicationTests.java
+++ src/test/java/com/cwc/CojCodeSandboxApplicationTests.java
@@ -3,19 +3,10 @@ package com.cwc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Scanner;
-
 @SpringBootTest
 class CojCodeSandboxApplicationTests {
 
     @Test
     void contextLoads() {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a + b);
     }
-
-
-
 }
