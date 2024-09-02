7535d3c0339994d6c72850a52a39bfb82fbad66e
Morii <2021694476@qq.com>
2024-09-02 09:41:43 +0800

    Update MyResultCallback.java

--- src/main/java/com/cwc/codesandbox/resultcallback/MyResultCallback.java
+++ src/main/java/com/cwc/codesandbox/resultcallback/MyResultCallback.java
@@ -3,16 +3,18 @@ package com.cwc.codesandbox.resultcallback;
 import com.cwc.model.ExecuteMessage;
 import com.github.dockerjava.api.async.ResultCallback;
 import com.github.dockerjava.api.model.Statistics;
+
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.Closeable;
 
 @Slf4j
 public class MyResultCallback implements ResultCallback<Statistics> {
-    private ExecuteMessage executeMessage;
+    private final ExecuteMessage executeMessage;
 
     public MyResultCallback(ExecuteMessage executeMessage) {
         this.executeMessage = executeMessage;
+        executeMessage.setMemory(0L);
     }
 
     @Override
@@ -26,11 +28,8 @@ public class MyResultCallback implements ResultCallback<Statistics> {
     @Override
     public void onNext(Statistics statistics) {
         Long usage = statistics.getMemoryStats().getUsage();
-        log.info("onNext usage: {}", usage);
         if (usage != null) {
             executeMessage.setMemory(Math.max(executeMessage.getMemory(), usage));
-            log.info("usage: {}", usage);
-            log.info(String.valueOf(executeMessage.getMemory()));
         }
     }
 
